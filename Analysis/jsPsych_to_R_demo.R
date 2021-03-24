eb908045c68fea3138e35d19049315e14f4a6cf6
Jeffrey K. Bye <jeffbye@gmail.com>
2021-03-24 15:48:14 -0500

    read in multiple data files

--- Analysis/jsPsych_to_R_demo.R
+++ Analysis/jsPsych_to_R_demo.R
@@ -1,10 +1,10 @@
 # jsPsych demo
 
-source("jspsych2r/Analysis/jsPsych_to_R_functions.R")
+source("jsPsych_to_R_functions.R")
 round
 parse_study
 
-ex_data <- read.csv("jspsych2r/Analysis/In_Data/epsy5200_jspsych_demo.csv")
+ex_data <- read.csv("In_Data/epsy5200_jspsych_demo.csv")
 ex_data
 
 coffee_options <- c('sugar', "cream", 'honey', 'nondairy milk', 'dairy milk', 'more coffee')
@@ -12,6 +12,18 @@ food_options <- c("Apples", "Bananas", "Carrots", "Donuts", "Eggplant")
 
 multi_list <- list(coffee_options, food_options)
 
-parsed <- ex_data %>% 
-  parse_study(multi_list)
-# oops, something is broken
+files <- list.files("In_Data", full.names = TRUE)
+
+parse_df <- NULL # initialization
+
+# for (i in files) {
+#   print(i)
+# }
+
+for (f in files) {
+  cur_data <- read.csv(f)
+  cur_parse <- parse_study(cur_data, multi_list, prefix = TRUE, rt = TRUE)
+  parse_df <- bind_rows(parse_df, cur_parse)
+}
+parse_df
+
